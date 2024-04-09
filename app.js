0a0093b1f6df0ae30505e110e16bb18f2fa2483b
LucasCarapella <166466582+LucasCarapella@users.noreply.github.com>
2024-04-09 08:26:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juego del número secreto"
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Indica un número del 1 al 10"
+
+function intento_de_usuario(){
+
+    alert ("Click desde el botón");
+}
